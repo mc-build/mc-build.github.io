df8359fab2d53453110f288dd375fc7b9ba98047
IanSSenne <IanSSenne@gmail.com>
2020-11-25 06:06:33 -0800

    change backend to docusaurus

---
+++ docusaurus.config.js
@@ -0,0 +1,73 @@
+module.exports = {
+  title: 'MC-BUILD',
+  tagline: 'A datapack build tool',
+  url: 'https://docs.mcbuild.dev',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'mc-build', // Usually your GitHub org/user name.
+  projectName: 'mc-build', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'MC-BUILD',
+      logo: {
+        alt: 'MC-BUILD Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+        },
+        {
+          href: 'https://github.com/mc-build',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            }
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/mc-build/docs',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
